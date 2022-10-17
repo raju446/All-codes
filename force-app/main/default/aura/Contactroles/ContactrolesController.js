6577d7b669e5323d67723113f6c84099aaaad7ab
raju446 <petluriraju@gmail.com>
2022-10-17 19:14:20 +0530

    Contact roles base on opportunity lightning aura
    Signed-off-by: raju446 <petluriraju@gmail.com>

---
+++ force-app/main/default/aura/Contactroles/ContactrolesController.js
@@ -0,0 +1,24 @@
+({
+ myAction : function(component, event, helper) 
+     {
+
+  component.set('v.columns', [
+            {label: 'Contact', fieldName: 'Contact__c', type: 'Lockup'},
+            {label: 'Role', fieldName: 'Role', type: 'Picklist'},
+            {label: 'Title', fieldName: 'Title__c', type: 'text'},
+         ]);
+
+var ConList = component.get("c.getRelatedList");
+   ConList.setParams
+         ({
+             recordId: component.get("v.recordId")
+         });
+   
+ ConList.setCallback(this, function(data) 
+                           {
+             component.set("v.ContactRole", data.getReturnValue());
+                           });
+
+         $A.enqueueAction(ConList);
+ }
+})
